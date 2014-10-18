0c98e0b0c563efb3be520aeb806986b6d3723f67
Zo0MER <exzo0mex@gmail.com>
2014-10-18 17:04:24 +0400

    Scene was updated

---
+++ Assets/Scripts/MainSphere.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainSphere : MonoBehaviour {
+
+	Vector3 initalPos;
+	bool isCanMove = false;
+	// Use this for initialization
+	void Start () {
+		initalPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isCanMove) 
+		{
+			transform.position = initalPos;
+			rigidbody2D.velocity = Vector2.zero;
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			isCanMove = true;
+			rigidbody2D.AddForce(new Vector3(100.0f, 0.0f, 0.0f));
+		}
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			isCanMove = false;
+		}
+	}
+}
