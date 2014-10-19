f6002ec5c2cf8bbeca1b09bba875af890039e0ae
Zo0MER <exzo0mex@gmail.com>
2014-10-19 06:37:34 +0400

    Buttons, portal was fixed

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Portal : MonoBehaviour {
 
+    private float angleSpeed = 5.0f;
+    
 	// Use this for initialization
 	void Start () {
         ((GameObject)GameObject.FindGameObjectWithTag("WinText")).GetComponent<TextMesh>().renderer.enabled = false;
@@ -10,7 +12,7 @@ public class Portal : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    transform.Rotate(new Vector3(0, 0, angleSpeed));
 	}
 
     void OnTriggerEnter2D(Collider2D c)
